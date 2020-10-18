e61a1cb1aa8336faa33139217dc34309b759cdcc
Christian Paranas <christiannparanas@gmail.com>
2020-10-19 02:39:17 +0800

    room

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,27 +1,26 @@
 import Head from "next/head";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
 import { useState } from "react";
 
 export default function Home() {
   const [username, setUsername] = useState("");
-  const router = useRouter()
+  const router = useRouter();
 
   const saveUsername = (e) => {
     e.preventDefault();
 
     // checks if user input username
     if (username.length === 0) {
-      alert('Please enter your name')
+      alert("Please enter your name");
       return;
     }
 
     // putting username value to localstorage to use it in database storing, not using any state mgm
     localStorage.setItem("username", username);
-    
-    // redirect to room onclick join btn
-    router.push('/room')
 
+    // redirect to room onclick join btn
+    router.push("/room");
   };
 
   return (
@@ -40,6 +39,7 @@ export default function Home() {
             onChange={(e) => setUsername(e.target.value)}
           />
           <input className="sub" type="submit" value="Join Room" />
+          <p>built by christianparanas</p>
         </form>
       </div>
     </div>
